79053274d48859259e7a47cde9f8072ac8d9ea1d
Fatma-Elzhraa Elsayed <63948537+fatma105@users.noreply.github.com>
2024-07-27 17:11:47 +0300

    Add files via upload

---
+++ AgeCalculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int BirthYear;
+    cout<<"Age calculator program:"<<endl;
+    cout<<"Enter the birth year =";
+    cin>>BirthYear;
+    int YourAge=2024-BirthYear;
+    cout<<"Your Age = "<<YourAge;
+    return 0;
+}
